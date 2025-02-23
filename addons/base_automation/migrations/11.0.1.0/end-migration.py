8fdda26cd1824f3312de85c392aeb95515996979
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-02-23 11:27:25 +0100

    [OU-FIX] base_automation: Avoid falsy values
    Doing `env.get()` of any model, even if valid, returns a falsy value,
    so all the rules will be always archived.
    
    For avoiding that, we check that the return type is an Odoo model
    instead of None.
    
    Closes #4764

--- addons/base_automation/migrations/11.0.1.0/end-migration.py
+++ addons/base_automation/migrations/11.0.1.0/end-migration.py
@@ -1,13 +1,14 @@
 # Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import models
 from openupgradelib import openupgrade
 
 
 def deactivate_invalid_model_rules(env):
     """Deactivate rules with invalid models."""
     for rule in env['base.automation'].search([]):
-        if not env.get(rule.model_name):
+        if not isinstance(env.get(rule.model_name), models.Model):
             rule.active = False
 
 
