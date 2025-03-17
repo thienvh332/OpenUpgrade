72290ee7b2dd3249fe091947cd27a897e03e6577
Holger Brunn <mail@hunki-enterprises.com>
2025-03-17 10:30:15 +0100

    [OU-ADD] mail

---
+++ openupgrade_scripts/scripts/mail/18.0.1.18/pre-migration.py
@@ -0,0 +1,27 @@
+# Copyright 2025 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+model_renames = [
+    ("mail.notification.web.push", "web.push"),
+    ("mail.partner.device", "mail.push.device"),
+    ("mail.shortcode", "mail.canned.response"),
+]
+
+table_renames = [
+    ("mail_notification_web_push", "web_push"),
+    ("mail_partner_device", "mail_push_device"),
+    ("mail_shortcode", "mail_canned_response"),
+]
+
+field_renames = [
+    ("web.push", "web_push", "user_device", "mail_push_device_id"),
+]
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_models(env.cr, model_renames)
+    openupgrade.rename_tables(env.cr, table_renames)
+    openupgrade.rename_fields(env, field_renames)
