da89d75535125c31680d516b4901a1aba98320ee
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-04-03 16:08:50 +0200

    [OU-IMP] note > project_todo: Finish migration scripts
    TT50089

--- openupgrade_scripts/scripts/project_todo/17.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/project_todo/17.0.1.0/pre-migration.py
@@ -1,14 +1,8 @@
 # Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
+# Copyright 2025 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from openupgradelib import openupgrade
-
-_rename_xmlids = [
-    (
-        "note.mail_activity_data_reminder",
-        "project_todo.mail_activity_data_reminder",
-    ),
-]
 
 
 def _convert_note_tag_to_project_tags(env):
@@ -27,13 +21,15 @@ def _convert_note_tag_to_project_tags(env):
 
 def _convert_note_note_to_project_task(env):
     openupgrade.logged_query(
+        env.cr, "ALTER TABLE project_task ADD COLUMN old_note_id INTEGER"
+    )
+    openupgrade.logged_query(
         env.cr,
         """
-        ALTER TABLE project_task ADD COLUMN openupgrade_legacy_note_id INTEGER;
         INSERT INTO project_task(
             create_uid, write_uid, create_date, write_date,
             active, name, description, priority, sequence, state, project_id,
-            display_in_project, company_id, color, openupgrade_legacy_note_id
+            display_in_project, company_id, color, old_note_id
         )
         SELECT create_uid, write_uid, create_date, write_date,
                open, name, memo, '0', sequence, '01_in_progress', null,
@@ -48,13 +44,10 @@ def _fill_project_tags(env):
         env.cr,
         """
         INSERT INTO project_tags_project_task_rel (project_task_id, project_tags_id)
-        SELECT
-            project_task.id AS project_task_id,
-            project_tags.id AS project_tags_id
-        FROM note_tags_rel
-        JOIN note_note ON note_tags_rel.note_id = note_note.id
-        JOIN project_task ON project_task.openupgrade_legacy_note_id = note_note.id
-        JOIN note_tag ON note_tags_rel.tag_id = note_tag.id
+        SELECT project_task.id, project_tags.id
+        FROM note_tags_rel rel
+        JOIN project_task ON project_task.old_note_id = rel.note_id
+        JOIN note_tag ON rel.tag_id = note_tag.id
         JOIN project_tags ON project_tags.name = note_tag.name;
         """,
     )
@@ -62,12 +55,14 @@ def _fill_project_tags(env):
 
 def _fill_stage_for_todo_task(env):
     openupgrade.logged_query(
+        env.cr, "ALTER TABLE project_task_type ADD COLUMN old_note_stage_id INTEGER"
+    )
+    openupgrade.logged_query(
         env.cr,
         """
-        ALTER TABLE project_task_type ADD COLUMN note_stage_id INTEGER;
         INSERT INTO project_task_type (
             create_uid, write_uid, create_date, write_date, active,
-            user_id, sequence, name, fold, note_stage_id
+            user_id, sequence, name, fold, old_note_stage_id
         )
         SELECT
             create_uid, write_uid, create_date, write_date, true,
@@ -86,18 +81,8 @@ def _fill_stage_for_todo_task(env):
             project_task_type.user_id,
             project_task_type.id stage_id
         FROM note_stage_rel rel
-        JOIN project_task ON project_task.openupgrade_legacy_note_id = rel.note_id
-        JOIN project_task_type ON project_task_type.note_stage_id = rel.stage_id
-        """,
-    )
-
-
-def _drop_tmp_columns(env):
-    openupgrade.logged_query(
-        env.cr,
-        """
-        ALTER TABLE project_task DROP COLUMN openupgrade_legacy_note_id;
-        ALTER TABLE project_task_type DROP COLUMN note_stage_id;
+        JOIN project_task ON project_task.old_note_id = rel.note_id
+        JOIN project_task_type ON project_task_type.old_note_stage_id = rel.stage_id
         """,
     )
 
@@ -108,8 +93,4 @@ def migrate(env, version):
     _convert_note_note_to_project_task(env)
     _fill_project_tags(env)
     _fill_stage_for_todo_task(env)
-    openupgrade.merge_models(
-        env.cr, "note.note", "project.task", "openupgrade_legacy_note_id"
-    )
-    _drop_tmp_columns(env)
-    openupgrade.rename_xmlids(env.cr, _rename_xmlids)
+    openupgrade.merge_models(env.cr, "note.note", "project.task", "old_note_id")
