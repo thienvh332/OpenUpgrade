da17e9d2cd68d0a675d6080aced0e514ff8c8736
Miquel Raïch <miquel.raich@forgeflow.com>
2025-05-30 15:39:43 +0200

    [OU-FIX] hr: finalize migration

--- openupgrade_scripts/scripts/hr/18.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/hr/18.0.1.1/post-migration.py
@@ -1,10 +1,5 @@
 from openupgradelib import openupgrade
 
-_deleted_xml_records = [
-    "hr.action_hr_employee_create_user",
-    "hr.hr_presence_control_login",
-]
-
 
 @openupgrade.migrate()
 def migrate(env, version):
@@ -12,16 +7,4 @@ def migrate(env, version):
     openupgrade.delete_record_translations(
         env.cr, "hr", ["contract_type_part_time"], ["name"]
     )
-    openupgrade.delete_records_safely_by_xml_id(
-        env,
-        _deleted_xml_records,
-    )
-
-    # Set hr_presence_state to 'archive' where employee is not active
-    employees = (
-        env["hr.employee"]
-        .with_context(active_test=False)
-        .search([("active", "=", False)])
-    )
-    for emp in employees:
-        emp.hr_presence_state = "archive"
+    openupgrade.delete_records_safely_by_xml_id(env, ["hr.hr_presence_control_login"])
--- openupgrade_scripts/scripts/hr/18.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/hr/18.0.1.1/pre-migration.py
@@ -1,42 +1,5 @@
 from openupgradelib import openupgrade
 
-
-def _column_exists(cr, table, column):
-    cr.execute(
-        """
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name = %s AND column_name = %s
-    """,
-        (table, column),
-    )
-    return cr.fetchone() is not None
-
-
-def _update_hr_presence_state(env):
-    table = "hr_employee"
-    column = "hr_presence_state"
-
-    if _column_exists(env.cr, table, column):
-        # Copy old column values to backup legacy column
-        openupgrade.copy_columns(
-            env.cr,
-            {
-                table: [(column, None, None)],
-            },
-        )
-
-        # Map old value to new
-        old_column = openupgrade.get_legacy_name(column)
-        openupgrade.map_values(
-            env.cr,
-            old_column,
-            column,
-            [("to_define", "out_of_working_hour")],
-            table=table,
-        )
-
-
 _new_columns = [
     (
         "hr.employee",
@@ -45,11 +8,12 @@ _new_columns = [
         "kilometers",
         "hr_employee",
     ),
-]
-
-_new_fields = [
-    ("is_flexible", "hr.employee", "hr_employee", "boolean", False, "hr"),
-    ("is_fully_flexible", "hr.employee", "hr_employee", "boolean", False, "hr"),
+    ("hr.employee", "is_flexible", "boolean", None, "hr_employee"),
+    ("hr.employee", "is_fully_flexible", "boolean", None, "hr_employee"),
+    ("res.company", "hr_presence_control_attendance", "boolean"),
+    ("res.company", "hr_presence_control_email", "boolean"),
+    ("res.company", "hr_presence_control_ip", "boolean"),
+    ("res.company", "hr_presence_control_login", "boolean", True),
 ]
 
 
@@ -59,16 +23,25 @@ def migrate(env, version):
         env.cr, {"employee_category_rel": [("emp_id", "employee_id")]}
     )
     openupgrade.add_columns(env, _new_columns)
-    openupgrade.add_fields(env, _new_fields)
-    _update_hr_presence_state(env)
     openupgrade.logged_query(
         env.cr,
         """
         UPDATE hr_employee
-        SET is_flexible = false, is_fully_flexible = false
+        SET is_fully_flexible = CASE WHEN resource_calendar_id IS NULL
+                                     THEN TRUE ELSE FALSE END
         """,
     )
-
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_employee he2
+        SET is_flexible = CASE WHEN he.is_fully_flexible OR rc.flexible_hours
+                               THEN TRUE ELSE FALSE END
+        FROM hr_employee he
+        LEFT JOIN resource_calendar rc ON he.resource_calendar_id = rc.id
+        WHERE he.id = he2.id
+        """,
+    )
     openupgrade.logged_query(
         env.cr,
         """
