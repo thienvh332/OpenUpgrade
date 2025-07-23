c1547c4597fda0058c6a5144aa6dcfa05ad45fc2
Miquel Raïch <miquel.raich@forgeflow.com>
2025-07-23 19:05:19 +0200

    [OU-ADD] l10n_dk

---
+++ openupgrade_scripts/scripts/l10n_dk/17.0.1.3/post-migration.py
@@ -0,0 +1,8 @@
+# Copyright 2025 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env, "l10n_dk", "17.0.1.3/noupdate_changes.xml")
